812856a5b7e1ca938f9592cacd318d54545c9b31
RaylaTheBrave <lorygiannetti@gmail.com>
2026-09-02 23:35:18 +0200

    Test rework on Dragon Fly Ability

--- test/battle/rogue_ability/dragon_fly.c
+++ test/battle/rogue_ability/dragon_fly.c
@@ -1,3 +1,4 @@
+/*
 #include "global.h"
 #include "test/battle.h"
 
@@ -60,5 +61,39 @@ SINGLE_BATTLE_TEST("Dragon Fly increases Dragon-type move damage", s16 damage)
         EXPECT_EQ(results[0].damage, results[1].damage); // Tackle should be unaffected
         EXPECT_MUL_EQ(results[2].damage, Q_4_12(1.5), results[3].damage); // Dragon Claw should be affected
         EXPECT_MUL_EQ(results[4].damage, Q_4_12(1.5), results[5].damage); // Dragon Pulse should be affected
+    }
+}
+*/
+#include "global.h"
+#include "test/battle.h"
+
+SINGLE_BATTLE_TEST("Dragon Fly increases Bug-type move damage", s16 damage)
+{
+    u32 move;
+    u16 ability;
+
+    PARAMETRIZE { move = MOVE_TACKLE; ability = ABILITY_LEVITATE; }
+    PARAMETRIZE { move = MOVE_TACKLE; ability = ABILITY_DRAGON_FLY; }
+    PARAMETRIZE { move = MOVE_X_SCISSOR; ability = ABILITY_LEVITATE; }
+    PARAMETRIZE { move = MOVE_X_SCISSOR; ability = ABILITY_DRAGON_FLY; }
+    PARAMETRIZE { move = MOVE_BUG_BUZZ; ability = ABILITY_LEVITATE; }
+    PARAMETRIZE { move = MOVE_BUG_BUZZ; ability = ABILITY_DRAGON_FLY; }
+
+    GIVEN {
+        ASSUME(gBattleMoves[MOVE_TACKLE].type != TYPE_BUG);
+        ASSUME(gBattleMoves[MOVE_X_SCISSOR].type == TYPE_BUG);
+        ASSUME(gBattleMoves[MOVE_BUG_BUZZ].type == TYPE_BUG);
+        ASSUME(gBattleMoves[MOVE_X_SCISSOR].split == SPLIT_PHYSICAL);
+        ASSUME(gBattleMoves[MOVE_BUG_BUZZ].split == SPLIT_SPECIAL);
+        PLAYER(SPECIES_FLYGON) { Ability(ability); }
+        OPPONENT(SPECIES_SNORLAX);
+    } WHEN {
+        TURN { MOVE(player, move); }
+    } SCENE {
+        HP_BAR(opponent, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_EQ(results[0].damage, results[1].damage); // Tackle should be unaffected
+        EXPECT_MUL_EQ(results[2].damage, Q_4_12(1.5), results[3].damage); // Anchor Shot should be affected
+        EXPECT_MUL_EQ(results[4].damage, Q_4_12(1.5), results[5].damage); // Flash Cannon should be affected
     }
 }
