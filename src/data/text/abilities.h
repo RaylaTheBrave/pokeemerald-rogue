4ecad338be2e02170d704e56bd8ad1768936bb27
RaylaTheBrave <lorygiannetti@gmail.com>
2026-09-04 06:44:07 +0200

    Added Membrane Test

--- src/data/text/abilities.h
+++ src/data/text/abilities.h
@@ -308,8 +308,8 @@ static const u8 sSpicySprayDescription[] = _("Burns the foe when damaged.");
 static const u8 sFireManeDescription[] = _("Ups Fire-type moves.");
 
 static const u8 sForecastPriorityDescription[] = _("Weather moves go first.");
-static const u8 sDragonFlyDescription[] = _("Turns Dragon into Bug; +STAB");
-static const u8 sMembraneDescription[] = _("Turns Dragon into Bug; +STAB");
+static const u8 sDragonFlyDescription[] = _("Turns Dragon into Bug +STAB.");
+static const u8 sMembraneDescription[] = _("Ups Bug-type moves.");
 
 #if B_EXPANDED_ABILITY_NAMES == TRUE
 const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
